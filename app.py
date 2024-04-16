0c733039d742cdff3bc79e7525146c16bbcbaa3b
gllrmoliva <gllrmoliva42@gmail.com>
2024-04-15 23:08:37 -0400

    add: inicio de sesión con usuarios falsos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template,request, url_for
-
+import database
 app = Flask(__name__)
 
 # Pagina de inicio de sesión
@@ -9,20 +9,28 @@ def signin():
         return render_template("signin.html")
     elif request.method == 'POST':
         request_form = request.form
+        # Esto es solo para el primer sprint
         if 'signinstudent' in request_form:
-            return redirect(url_for('homestudent')) 
+            if (database.studentInDB(request_form['email'], request_form['password'])):
+                return redirect(url_for('homestudent'))
+            else:
+                return render_template("signin.html", error_message = 'Email o Contraseña equivocado, intente con otro')
+
         elif 'signintutor' in request_form:
-            return redirect(url_for('hometutor')) 
+            if (database.tutorInDB(request_form['email'], request_form['password'])):
+                return redirect(url_for('hometutor'))
+            else:
+                return render_template("signin.html", error_message = 'Email o Contraseña equivocado, intente con otro')
     
 # pagina de cursos profesor, quizas cambiar el nombre a /tutor/cursos
 @app.route("/tutor", methods = ['GET', 'POST'])
 def hometutor():
     if request.method == 'GET':
         # Claramente el Cursos, debe ser sacado con una función de backend la cual de de output los cursos, con sus caracteristicas
-        return render_template('tutor/home.html',cursos = ['Lenguaje', 'Matematicas', 'Historia'])
+        return render_template('tutor/home.html',cursos = ['Lenguaje', 'Matemáticas', 'Historia'])
 
 #pagina de cursos alumno, quizas cambiar el nombre a /student/cursos
 @app.route("/student", methods = ['GET', 'POST'])
 def homestudent():
     if request.method == 'GET':
-        return render_template('student/home.html',cursos = ['Lenguaje', 'Matematicas', 'Historia'])
+        return render_template('student/home.html',cursos = ['Lenguaje', 'Matemáticas'])
