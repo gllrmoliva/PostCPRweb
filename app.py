03f69e8bf38d9c63e3fae5924093e385c49db317
gllrmoliva <gllrmoliva42@gmail.com>
2024-04-23 20:21:55 -0400

    sampletext

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ def signin():
                 return redirect(url_for('hometutor'))
             else:
                 return render_template("signin.html", error_message = 'Email o Contraseña equivocado, intente con otro')
+
+# RUTAS TUTOR
     
 # pagina de cursos profesor, quizas cambiar el nombre a /tutor/cursos
 @app.route("/tutor", methods = ['GET', 'POST'])
@@ -29,8 +31,32 @@ def hometutor():
         # Claramente el Cursos, debe ser sacado con una función de backend la cual de de output los cursos, con sus caracteristicas
         return render_template('tutor/home.html',cursos = ['Lenguaje', 'Matemáticas', 'Historia'])
 
+# TODO:hay que implementar esto aaaaaaaaaaaaaa
+@app.route("/tutor/c/<int:courseid>", methods = ['GET', 'POST'])
+def coursetutor():
+    if request.method == 'GET':
+        return render_template('tutor/course.html')
+    elif request.method == 'POST':
+        return render_template('algo')
+
+# RUTAS STUDIANTE
 #pagina de cursos alumno, quizas cambiar el nombre a /student/cursos
 @app.route("/student", methods = ['GET', 'POST'])
 def homestudent():
     if request.method == 'GET':
         return render_template('student/home.html',cursos = ['Lenguaje', 'Matemáticas'])
+
+# TODO:hay que implementar esto aaaaaaaaaaaaaa
+@app.route("/student/c/<int:courseid>", methods = ['GET', 'POST'])
+def coursestudent():
+    if request.method == 'GET':
+        return render_template('student/course.html')
+    elif request.method == 'POST':
+        return render_template('algo')
+
+@app.route("/student/h", methods = ['GET', 'POST'])
+def homeworksstudent():
+    if request.method == 'GET':
+        return render_template('student/homeworks.html')
+    elif request.method == 'POST':
+        return render_template('algo')
