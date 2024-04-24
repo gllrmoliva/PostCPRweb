bec4dd4c1eb53d9c0884bf5b94d23a6fd95da4dd
gllrmoliva <gllrmoliva42@gmail.com>
2024-04-24 18:40:41 -0400

    Add: Creación de cursos desde home-tutor

--- database.py
+++ database.py
@@ -6,6 +6,9 @@ tutor = { 'email':'tutor@tutor.com',
             'password':'5678'
 }
 
+tareas = []
+
+cursos = []
 
 def studentInDB(email, password):
     if (student['email'] == email) and (student['password'] == password):
@@ -18,3 +21,22 @@ def tutorInDB(email, password):
         return True
     else:
         return False
+
+def get_tareas():
+    return tareas
+
+def get_cursos():
+    return cursos
+
+
+
+# OJo, criterios es una lista de criterios
+def crear_tarea(id, nombre, descripcion, criterios):
+    tarea = {'id': id, 'nombre':nombre , 'descripcion': descripcion, 'criterios': criterios }
+    tareas.append(tarea)
+
+def crear_curso(id, nombre, descripcion):
+    curso = {'id': id, 'nombre':nombre , 'descripcion': descripcion, 'tareas':[]}
+    cursos.append(curso)
+    
+
