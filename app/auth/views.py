5c5c1502f2df6f33fb35ef9aa8b886cce8d562ba
gllrmoliva <gllrmoliva42@gmail.com>
2024-04-27 04:01:46 -0400

    Conectando tutor home y student home con base de datos

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,40 +13,57 @@ muestra un mensaje tipo no se pudo iniciar sesion. Lo mismo ocurre con estudiant
 
 @auth.route("/", methods = ['GET', 'POST'])
 def signin():
+    # Si se accede con el metodo get se renderiza la pagina html (templates/signin.html)
     if request.method == 'GET':
         return render_template("signin.html")
 
+    # Al accederse con el metodo post
     elif request.method == 'POST':
+        # Recuperamos los datos del formulario de inicio de sesión enviados
         request_form = request.form
 
         email = request_form['email']
         password = request_form['password']
 
+        # Iniciamos la base de datos, además aqui se estan creando las tablas y añadiendo datos de prueba
         database = db.Connection()
         database.connect()
+        database.create_tables()
+        database.fill_tables_with_examples()
         # tengo miedo sobre que pasa si no se cierra la sesión
 
+        # obtenemos al usuario de la base de datos a partir del email, y lo guardamos
         user = database.get_user_from_email(email)
 
+        # Si el usuario no existe o la contraseña no es la misma que en la base de datos
+        # renderizamos signin.html, pero esta vez mostrando un mensaje, esto lo hacemos a traves del comando
+        # flash, el cual luego es manejado en el html con jinja
         if not user or not (user['password'] == password):
             flash('Email o Contraseña equivocado, intente con otro')
             return redirect(url_for('auth.signin'))
         else:
-            session['user_id'] = user['id']
-            
+            # Si el usuario existe y la contraseña esta correcta, vemos cual de los botones se apretaron
+            # si se apreto el boton de iniciar sesión como estudiante, verificamos que efectivamente el usuario es un
+            # estudiante y guardamos el id y el tipo de usuario en una variable global de flask llamada session, esta variable se 
+            # puede utilizar en cualquier ruta de la web, para el boton iniciar sesion como tutor se hace lo mismo
             if 'signinstudent' in request_form and database.is_student(user):
-                session['user_id'] = 'student'
+                session['user_id'] = user['id']
+                session['user_type'] = 'student'
                 return redirect(url_for('student.homestudent'))
-            elif 'signintutor' in request_form and user['type'] == 'tutor':
-                session['user_id'] = 'tutor'
+            elif 'signintutor' in request_form and database.is_tutor(user):
+                session['user_id'] = user['id']
+                session['user_type'] = 'tutor'
                 return redirect(url_for('tutor.hometutor'))
+            
+            # Si es que por alguna razon el usuario no es tutor ni estudiante, entonces lo atajamos aquí, 
+            # igualmente creo que esta función es media inutil, ya que nunca se deberia llegar hasta aquí
             else:
-                flash('Email o Contraseña equivocado, intente con otro')
+                flash('Email o Contraseña equivocado, intente con otro (tipo de usuario no definido)')
                 return redirect(url_for('auth.signin'))
 
+# Ruta a la que se accede cuando se quiere cerrar sesión
 @auth.route("/logout")
 def logout():
-    session.pop('user_id', None)
-    session.pop('user_type', None)
+    session.clear()
     flash('Sesión cerrada correctamente.', 'info')
     return redirect(url_for('auth.signin'))
