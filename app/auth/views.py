cd0323c14e9350d1577392202928662b946e4295
gllrmoliva <gllrmoliva42@gmail.com>
2024-04-28 00:14:20 -0400

    merging

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,9 @@
 from . import auth 
-from flask import render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask import render_template, request, redirect, url_for, flash, session
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from database import db
-
-import fakedatabase
 
 """
 Aquí se puede iniciar sesión como estudiante o tutor. Si se apreta el botón iniciar sesión como tutor, se verifica
@@ -24,25 +22,31 @@ def signin():
         email = request_form['email']
         password = request_form['password']
 
-        # si quiero hashear la contraseña
-        # password = generate_password_hash(request_form['password'], method="pbkdf2") 
-
-        # Quizas se deba refactorizar despues
-        # TODO: ver como se va a cerrar la base de datos
         database = db.Connection()
         database.connect()
+        # tengo miedo sobre que pasa si no se cierra la sesión
 
         user = database.get_user_from_email(email)
 
-        # verificamos que el usuario exista y la contraseña este correcta        
         if not user or not (user['password'] == password):
             flash('Email o Contraseña equivocado, intente con otro')
             return redirect(url_for('auth.signin'))
         else:
-            if ('signinstudent' in request_form) and (database.is_student(user)):
-                    return redirect(url_for('student.homestudent'))
-            elif ('signintutor' in request_form) and (database.is_tutor(user)):
-                    return redirect(url_for('tutor.hometutor'))
+            session['user_id'] = user['id']
+            
+            if 'signinstudent' in request_form and database.is_student(user):
+                session['user_id'] = 'student'
+                return redirect(url_for('student.homestudent'))
+            elif 'signintutor' in request_form and user['type'] == 'tutor':
+                session['user_id'] = 'tutor'
+                return redirect(url_for('tutor.hometutor'))
             else:
                 flash('Email o Contraseña equivocado, intente con otro')
                 return redirect(url_for('auth.signin'))
+
+@auth.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    session.pop('user_type', None)
+    flash('Sesión cerrada correctamente.', 'info')
+    return redirect(url_for('auth.signin'))
