33bd0c326909113803040c1702bf3f8c061066e7
nitrolanco <mamibalss@gmail.com>
2024-06-01 18:05:28 -0400

    Añadido backend reviews y reviewtask student; corregido un error

--- database/schemas.py
+++ database/schemas.py
@@ -41,7 +41,8 @@ class Database:
 
     def add_course(self, name, user):
         with self.Session() as session:
-            course = Course(name=name, tutor_id=user.id)
+            tutor = session.query(Tutor).filter(Tutor.user_id == user.id).first()
+            course = Course(name=name, tutor_id=tutor.tutor_id)
             session.add(course)
             session.commit()
 
@@ -68,4 +69,153 @@ class Database:
         with self.Session() as session:
             return (
                 session.query(Criterion).filter(Criterion.task_id == task.task_id).all()
+            )
+
+    def create_task(self, name, instructions, course):
+        with self.Session() as session:
+            task = Task(
+                name=name, instructions=instructions, course_id=course.course_id
+            )
+            session.add(task)
+            session.commit()
+
+    def get_all_criteria_from_task(self, task_id):
+        with self.Session() as session:
+            return session.query(Criterion).filter(Criterion.task_id == task_id).all()
+
+    def get_submission_from_task(self, task_id, user_id):
+        with self.Session() as session:
+            student_id = (
+                session.query(Student).filter(Student.user_id == user_id).first()
+            )
+            return (
+                session.query(Submission)
+                .filter(
+                    Submission.task_id == task_id
+                    and Submission.student_id == student_id
+                )
+                .first()
+            )
+
+    def create_review(self, date, submission, user_id, is_pending=True):
+        with self.Session() as session:
+            review = Review(
+                date=date,
+                is_pending=False,
+                reviewer_id=user_id,
+                submission_id=submission.submission_id,
+            )
+            session.add(review)
+            session.commit()
+
+    def get_criterion_by_name(self, name, task_id):
+        with self.Session() as session:
+            return (
+                session.query(Criterion)
+                .filter(Criterion.name == name, Criterion.task_id == task_id)
+                .first()
+            )
+
+    def get_last_review(self):
+        with self.Session() as session:
+            return session.query(Review).order_by(Review.review_id.desc()).first()
+
+    def create_review_criterion(self, review_id, criterion_id, score):
+        with self.Session() as session:
+            review_criterion = ReviewCriterion(
+                review_id=review_id,
+                criterion_id=criterion_id,
+                valoracion=score,
+            )
+            session.add(review_criterion)
+            session.commit()
+
+    def is_review_reviewed(self, task_id, user_id):
+        with self.Session() as session:
+            submission = (
+                session.query(Submission).filter(Submission.task_id == task_id).first()
+            )
+
+            review = (
+                session.query(Review)
+                .filter(
+                    Review.submission_id == submission.submission_id
+                    and Review.reviewer_id == user_id
+                )
+                .first()
+            )
+            print(f"# Is pending: {review.is_pending}\n  ")
+            if review is None:
+                return False
+            else:
+                if review.is_pending is True:
+                    return False
+                elif review.is_pending is False:
+                    return True
+
+    def get_reviews_to_review(self, user_id):
+        with self.Session() as session:
+            return (
+                session.query(Review)
+                .filter(Review.reviewer_id == user_id and Review.is_pending == True)
+                .all()
+            )
+
+    def get_tasks_to_review(self, reviews):
+        tasks = []
+        with self.Session() as session:
+            for review in reviews:
+                submission = (
+                    session.query(Submission)
+                    .filter(Submission.submission_id == review.submission_id)
+                    .first()
+                )
+                task = (
+                    session.query(Task)
+                    .filter(Task.task_id == submission.task_id)
+                    .first()
+                )
+                tasks.append(task)
+            return tasks
+
+    def mark_submission_as_reviewed(self, task_id, user_id):
+        with self.Session() as session:
+            student = session.query(Student).filter(Student.user_id == user_id).first()
+            submission = (
+                session.query(Submission)
+                .filter(
+                    Submission.student == student.student_id,
+                    Submission.task_id == task_id,
+                )
+                .first()
+            )
+            return None
+
+    def mark_review_as_reviewed(self, submission, user_id):
+        with self.Session() as session:
+            review = (
+                session.query(Review)
+                .filter(
+                    Review.reviewer_id == user_id,
+                    Review.submission_id == submission.submission_id,
+                )
+                .first()
+            )
+            review.is_pending = False
+            session.commit()
+
+    def get_review(self, review_id):
+        with self.Session() as session:
+            return session.query(Review).filter(Review.review_id == review_id).first()
+
+    def get_submission_by_review(self, review_id):
+        with self.Session() as session:
+            submission_id = (
+                session.query(Review).filter(Review.review_id == review_id).first()
+            ).submission_id
+
+            return (
+                session.query(Submission)
+                .filter(Submission.submission_id == submission_id)
+                .first()
             )
