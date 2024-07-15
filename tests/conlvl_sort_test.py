3726f74aef516d737c417a1394cd4d320bbb10a4
square0108 <martinmariollanos@gmail.com>
2024-07-15 01:28:31 -0400

    feat: module tests para juntar unit tests que no necesiten correr app flask

---
+++ tests/conlvl_sort_test.py
@@ -0,0 +1,9 @@
+from .context import database
+from database.database import *
+
+def conlvl_sort_test():
+    test_db = Database()
+    print()
+    print("placeholder")
+
+conlvl_sort_test()
--- trustsystem/debugging.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("hi")
-
-    return 0
-
-main()
