0b6421565b5e6f4c880f5003f5bace3515118394
square0108 <martinmariollanos@gmail.com>
2024-07-15 17:58:40 -0400

    dev-feat (?): modulo de tests para realizar unit tests/pruebas pequeñas

--- tests/conlvl_sort_test.py
+++ tests/conlvl_sort_test.py
@@ -1,9 +1,21 @@
 from .context import database
 from database.database import *
+from database.example_values import insert_default_values
+from trustsystem.PCPRtrustrank import get_conflictsorted_submissions
+
+def create_test_db(database: Database):
+    database.init()
+    database.clear()
+    insert_default_values(database)
+    database.commit_changes()
+    database.close()
 
 def conlvl_sort_test():
-    test_db = Database()
-    print()
+    db = Database()
+    create_test_db(db)
+    test_task: Task = db.get_from_id(Task, 1)
+    print(test_task.name)
+    get_conflictsorted_submissions(test_task)
     print("placeholder")
 
 conlvl_sort_test()
